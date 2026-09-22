f1bb71d33ca6afcd255e152ca47f6199cc308551
Mohit-d13 <mohitdamle14@gmail.com>
2026-09-22 16:10:29 +0530

    configure s3 backend

--- backend.tf
+++ backend.tf
@@ -5,6 +5,6 @@ terraform {
     region       = "ap-south-1"
     use_lockfile = true
     encrypt      = true
-    kms_key_id   = "arn:aws:kms:ap-south-1:656338545145:key/707c388a-3a53-4bac-b1db-78028e63a6d6"
+    kms_key_id   = "alias/terraform-statefile-bucket-656338545145-key"
   }
 }
